6176ade6c3063bf91dfac58a837cca0a637f514d
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-03-18 13:17:46 +0300

    Initial Commit

---
+++ IOS5-HW17-AlexeyDavidenko/IOS5_HW17_AlexeyDavidenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS5_HW17_AlexeyDavidenkoApp.swift
+//  IOS5-HW17-AlexeyDavidenko
+//
+//  Created by Alexey Davidenko on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS5_HW17_AlexeyDavidenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
