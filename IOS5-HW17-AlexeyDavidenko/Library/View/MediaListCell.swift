69bcf7eac175dc6aa2c650f9bf6531d79ee3e8f2
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-04-03 19:01:40 +0300

    added MediaListModel and LibraryView

--- IOS5-HW17-AlexeyDavidenko/Library/View/MediaListCell.swift
+++ IOS5-HW17-AlexeyDavidenko/Library/View/MediaListCell.swift
@@ -7,28 +7,6 @@
 
 import SwiftUI
 
-struct Media: Identifiable {
-    var id = UUID()
-    let title: String
-    let image: String
-}
-
-class MediaListModel: ObservableObject {
-    @Published var medias: [Media] = [
-        Media(title: "Playlists", image: "music.mic"),
-        Media(title: "Artists", image: "music.note"),
-        Media(title: "Albums", image: "rectangle.stack.fill"),
-        Media(title: "Songs", image: "music.note"),
-        Media(title: "TV & Movies", image: "tv"),
-        Media(title: "Music Videos", image: "music.note.tv"),
-        Media(title: "Genres", image: "guitars"),
-        Media(title: "Compilations", image: "person.2.crop.square.stack"),
-        Media(title: "Composers", image: "music.quarternote.3"),
-        Media(title: "Downloaded", image: "arrow.down.circle"),
-        Media(title: "Home Sharing", image: "music.note.house"),
-    ]
-}
-
 struct MediaListCell: View {
     
     @StateObject var viewModel = MediaListModel()
